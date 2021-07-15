db2f66abbb1544f53e6cf47526c9fb1d2dc90a22
KunalPV <kpv904@gmail.com>
2021-07-15 17:55:35 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9a8a9c61-301f-40e4-a1d0-2aa8186cf047';
+//9a8a9c61-301f-40e4-a1d0-2aa8186cf047
+export const MODAL_CLOSE_SEC = 2.5;
